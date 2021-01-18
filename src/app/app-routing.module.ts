b270ec601a5380d2930c9390d0935d701a9ee6b4
LuVidmar <lucas.vidmar@gmail.com>
2021-01-18 02:12:10 -0300

    404, bootstrap and router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Notfound404Component } from './notfound404/notfound404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '404', component: Notfound404Component },
+  { path: '**',   redirectTo: '/404', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Material
+import { MaterialModule } from './material/material.module';
+
+//Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Notfound404Component } from './notfound404/notfound404.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Notfound404Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
